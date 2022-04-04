c34e7163b37eb05d80e36d620ba3b820d1def4e2
gelopfalcon <gelopfalcon@gmail.com>
2022-04-04 14:35:15 -0600

    Upload step1

---
+++ gif-movie-portal/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+        <p className="header"> 🎞️ Portal Películas Favoritas</p>
+          <p className="sub-text">
+            Colección de las películas favoritas de plazinautas ✨
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
