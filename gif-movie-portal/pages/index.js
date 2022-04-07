ae45d0bf1c4e855b0101c3408b0cc27dfdddec2a
gelopfalcon <gelopfalcon@gmail.com>
2022-04-07 14:07:37 -0600

    Change styles

--- gif-movie-portal/pages/index.js
+++ gif-movie-portal/pages/index.js
@@ -1,21 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import React, { useEffect, useState } from 'react';
 
 
 export default function Home() {
 
-  //1
   const MOVIES = [
     'https://bit.ly/3Jb3ae2',
     'https://bit.ly/3NOj69w'
   ]
 
   const [walletAddress, setWalletAddress] = useState(null);
-  //5
   const [inputValue, setInputValue] = useState('');
 
-  //9
   const [gifList, setGifList] = useState([]);
 
 
@@ -45,14 +40,11 @@ export default function Home() {
     }
   };
 
-  //2
   const renderConnectedContainer = () => (
-    <div className="connected-container">
-      {/* 4 */}
+    <div className="sol-connected-container">
       <form
         onSubmit={(event) => {
-          event.preventDefault();
-          //8
+          event.preventDefault()
           sendGif();
         }}
       >
@@ -60,15 +52,11 @@ export default function Home() {
           value={inputValue}
           onChange={onInputChange} />
 
-        <button type="submit" className="cta-button submit-gif-button">Enter</button>
+        <button type="submit" className="sol-cta-button sol-submit-gif-movie-button">Enter</button>
       </form>
-        {/* END 4 */}
-      <div className="gif-grid">
-        {/* 11 
-        {gifList.map((gif) => (
-        */}
-        {MOVIES.map(gif => (
-          <div className="gif-item" key={gif}>
+      <div className="sol-gif-grid-movies">
+        {gifList.map(gif => (
+          <div className="sol-gif-movie" key={gif}>
             <img src={gif} alt={gif} />
           </div>
         ))}
@@ -76,17 +64,17 @@ export default function Home() {
     </div>
   );
 
-  //6
+
   const onInputChange = (event) => {
     const { value } = event.target;
     setInputValue(value);
   };
 
-  //7
+
   const sendGif = async () => {
     if (inputValue.length > 0) {
       console.log('Gif link:', inputValue);
-      //12
+    
       setGifList([...gifList, inputValue]);
       setInputValue('');
     } else {
@@ -103,7 +91,6 @@ export default function Home() {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
-  //10
   useEffect(() => {
     if (walletAddress) {
       console.log('Fetching Movie list...');
@@ -128,7 +115,7 @@ export default function Home() {
 
   const renderNotConnectedContainer = () => (
     <button
-      className="cta-button connect-wallet-button"
+      className="sol-cta-button sol-connect-wallet-button"
       onClick={connectWallet}
     >
       Conectarse a la Wallet
@@ -138,15 +125,14 @@ export default function Home() {
 
   return (
 
-    <div className="App">
-      <div className="container">
+    <div className="app">
+      <div className="main-container">
         <div className="header-container">
-          <p className="header"> 🎞️ Portal Películas Favoritas</p>
+          <p className="header"> 🎞️ Películas Favoritas</p>
           <p className="sub-text">
-            Colección de las películas favoritas de plazinautas ✨
+            Colección de las películas favoritas de plazinautas
           </p>
           {!walletAddress && renderNotConnectedContainer()}
-          {/* 3 */}
           {walletAddress && renderConnectedContainer()}
         </div>
       </div>
