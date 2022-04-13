86e50cc43f6462ba9fb69ff43cc075bf36f49006
Falcon <>
2022-04-13 11:20:55 -0600

    Add step-8

--- gif-movie-portal/pages/index.js
+++ gif-movie-portal/pages/index.js
@@ -1,41 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import {IDL} from '../public/gif_movie_solana';
-import {Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
-import { Program, Provider, web3 } from '@project-serum/anchor';
+import React, { useEffect, useState } from "react";
+import { IDL } from "../public/gif_movie_solana";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
 
 export default function Home() {
-
-  const MOVIES = [
-    'https://bit.ly/3Jb3ae2',
-    'https://bit.ly/3NOj69w'
-  ]
+  const MOVIES = ["https://bit.ly/3Jb3ae2", "https://bit.ly/3NOj69w"];
 
   const [walletAddress, setWalletAddress] = useState(null);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const [gifList, setGifList] = useState([]);
-const programID = new PublicKey(IDL.metadata.address);
+  const programID = new PublicKey(IDL.metadata.address);
 
-const { SystemProgram, Keypair } = web3;
-const network = clusterApiUrl('devnet');
-const opts = {
-  preflightCommitment: "processed"
-}
+  const { SystemProgram, Keypair } = web3;
+  const network = clusterApiUrl("devnet");
+  const opts = {
+    preflightCommitment: "processed",
+  };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
 
-const getProvider = () => {
-  const connection = new Connection(network, opts.preflightCommitment);
-  const provider = new Provider(
-    connection, window.solana, opts.preflightCommitment,
-  );
-  return provider;
-}
-
-const stringToBytes = (input) => {
-  return new TextEncoder().encode(input);
-};
-
-
+  const stringToBytes = (input) => {
+    return new TextEncoder().encode(input);
+  };
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -43,15 +38,13 @@ const stringToBytes = (input) => {
 
       if (solana) {
         if (solana.isPhantom) {
-          console.log('Phantom wallet was found!');
-
+          console.log("Phantom wallet was found!");
 
           const response = await solana.connect({ onlyIfTrusted: true });
           setWalletAddress(response.publicKey.toString());
         }
-
       } else {
-        alert('Solana object not found! Install a Phantom Wallet 👻');
+        alert("Solana object not found! Install a Phantom Wallet 👻");
       }
     } catch (error) {
       console.error(error);
@@ -62,18 +55,27 @@ const stringToBytes = (input) => {
     <div className="sol-connected-container">
       <form
         onSubmit={(event) => {
-          event.preventDefault()
+          event.preventDefault();
           sendGif();
         }}
       >
-        <input type="text" placeholder="Ingresa tu película favorita"
+        <input
+          type="text"
+          placeholder="Ingresa tu película favorita"
           value={inputValue}
-          onChange={onInputChange} />
+          onChange={onInputChange}
+        />
 
-        <button type="submit" className="sol-cta-button sol-submit-gif-movie-button">Enter</button>
+        <button
+          type="submit"
+          className="sol-cta-button sol-submit-gif-movie-button"
+        >
+          Enter
+        </button>
       </form>
+
       <div className="sol-gif-grid-movies">
-        {gifList.map(gif => (
+        {gifList.map((gif) => (
           <div className="sol-gif-movie" key={gif.account.gifUrl}>
             <img src={gif.account.gifUrl} alt={gif.account.gifUrl} />
           </div>
@@ -82,74 +84,64 @@ const stringToBytes = (input) => {
     </div>
   );
 
-
   const onInputChange = (event) => {
     const { value } = event.target;
     setInputValue(value);
   };
 
-
   const sendGif = async () => {
     if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
+      console.log("Gif link:", inputValue);
 
       var provider = getProvider();
       var program = new Program(IDL, programID, provider);
       const [pda] = await PublicKey.findProgramAddress(
-      [
-        stringToBytes("gif_account"),
-        provider.wallet.publicKey.toBytes(),
-        stringToBytes(inputValue),
-      ],
-      program.programId
-    );
+        [
+          stringToBytes("gif_account"),
+          provider.wallet.publicKey.toBytes(),
+          stringToBytes(inputValue),
+        ],
+        program.programId
+      );
 
-    await program.rpc.initialize(inputValue, {
-      accounts: {
-        movieGif: pda,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-    });
+      await program.rpc.initialize(inputValue, {
+        accounts: {
+          movieGif: pda,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+      });
 
-    //3
-    getMovieList();
+      getMovieList();
 
-    setInputValue('');
+      setInputValue("");
     } else {
-      console.log('Empty input. Try again.');
+      console.log("Empty input. Try again.");
     }
-
   };
-
 
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
   }, []);
 
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching Movie list...');
+      console.log("Fetching Movie list...");
 
-//2
-  getMovieList()
+      getMovieList();
     }
   }, [walletAddress]);
 
-
-
   const connectWallet = async () => {
-
-
     const { solana } = window;
 
     if (solana) {
       const response = await solana.connect();
-      console.log('Connected with Public Key:', response.publicKey.toString());
+      console.log("Connected with Public Key:", response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
     }
   };
@@ -162,24 +154,59 @@ const stringToBytes = (input) => {
       Conectarse a la Wallet
     </button>
   );
-//1
-  const getMovieList = async() => {
+
+  const getMovieList = async () => {
     try {
       const provider = getProvider();
       const program = new Program(IDL, programID, provider);
       const getAllMovies = await program.account.movieGif.all();
-      console.log(getAllMovies[0].account.owner);
-      setGifList(getAllMovies)
-  
+      setGifList(getAllMovies);
     } catch (error) {
-      console.log("Error in getGifList: ", error)
+      console.log("Error in getGifList: ", error);
       setGifList(null);
     }
-  }
+  };
 
+  const getMoviesByOwner = async () => {
+    var provider = getProvider();
+    var program = new Program(IDL, programID, provider);
+
+    const gifsByOwner = await program.account.movieGif.all([
+      {
+        memcmp: {
+          bytes: provider.wallet.publicKey.toBase58(),
+          offset: 8,
+        },
+      },
+    ]);
+
+    console.log(gifsByOwner);
+    setGifList(gifsByOwner);
+  };
+
+  const showOptionsMovies = () => (
+    <div className="sol-show-movies-container">
+      <button
+        onClick={async () => {
+          getMoviesByOwner();
+        }}
+        className="sol-cta-button sol-submit-gif-movie-button"
+      >
+        Ver mis peliculas
+      </button>
+
+      <button
+        onClick={async () => {
+          getMovieList();
+        }}
+        className="sol-cta-button sol-submit-gif-movie-button"
+      >
+        Ver todas las peliculas
+      </button>
+    </div>
+  );
 
   return (
-
     <div className="app">
       <div className="main-container">
         <div className="header-container">
@@ -187,10 +214,11 @@ const stringToBytes = (input) => {
           <p className="sub-text">
             Colección de las películas favoritas de plazinautas
           </p>
-          {!walletAddress && renderNotConnectedContainer()}
-          {walletAddress && renderConnectedContainer()}
         </div>
+        {walletAddress && showOptionsMovies()}
+        {!walletAddress && renderNotConnectedContainer()}
+        {walletAddress && renderConnectedContainer()}
       </div>
     </div>
-  )
+  );
 }
