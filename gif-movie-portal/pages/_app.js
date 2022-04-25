c2cd1d61a64ce8d11eaf65c2ce86c5524660d078
Falcon <>
2022-04-25 14:38:00 -0600

    update form

--- gif-movie-portal/pages/_app.js
+++ gif-movie-portal/pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <main>
+        <nav className="border-b p-6">
+          <p className="text-4xl font-bold">Platzi Movies</p>
+          <div className="flex mt-4">
+            <Link href="/">
+              <a className="mr-4 link-menu">Inicio</a>
+            </Link>
+            <Link href="/add-movie">
+              <a className="mr-6 link-menu">Agregar Películas</a>
+            </Link>
+            <Link href="/my-movies">
+              <a className="mr-6 link-menu">Mis Películas</a>
+            </Link>
+          </div>
+        </nav>
+      </main>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- gif-movie-portal/pages/index.js
+++ gif-movie-portal/pages/index.js
@@ -1,18 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    
-    <div className="app">
-      <div className="main-container">
-        <div className="header-container">
-        <p className="header"> 🎞️ Películas Favoritas</p>
-          <p className="sub-text">
-            Colección de películas favoritas de plazinautas
-          </p>
-        </div>
-      </div>
-    </div>
-  )
+  return <div className="flex justify-center"></div>;
 }
